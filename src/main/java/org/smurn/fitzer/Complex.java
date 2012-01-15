f97b9c74d60848a2cc061542254f047151028d31
Stefan C. Mueller <scm@smurn.org>
2012-01-15 19:19:25 +0100

    Added Complex.
    Signed-off-by: Stefan C. Mueller <scm@smurn.org>

---
+++ src/main/java/org/smurn/fitzer/Complex.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2012 Stefan C. Mueller.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.smurn.fitzer;
+
+import java.math.BigDecimal;
+
+/**
+ * Represents a complex number.
+ */
+public class Complex {
+
+    private final BigDecimal real;
+    private final BigDecimal imag;
+
+    /**
+     * Creates an instance.
+     * @param real Real part of the complex number.
+     * @param imag Imaginary part of the complex number.
+     * @throws NullPointerException if {@code real} or {@code imag} is
+     * {@code null}.
+     */
+    public Complex(BigDecimal real, BigDecimal imag) {
+        if (real == null) {
+            throw new NullPointerException("Real part must not be null.");
+        }
+        if (imag == null) {
+            throw new NullPointerException("Imaginary part must not be null.");
+        }
+        this.real = real;
+        this.imag = imag;
+    }
+
+    /**
+     * Gets the imaginary part of this complex number.
+     * @return Imaginary part, never {@code null}.
+     */
+    public BigDecimal getImag() {
+        return imag;
+    }
+
+    /**
+     * Gets the real part of this complex number.
+     * @return Real part, never {@code null}.
+     */
+    public BigDecimal getReal() {
+        return real;
+    }
+}
---
+++ src/test/java/org/smurn/fitzer/ComplexTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2012 Stefan C. Mueller.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.smurn.fitzer;
+
+import java.math.BigDecimal;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for {@link Complex}.
+ */
+public class ComplexTest {
+
+    @Test(expected = NullPointerException.class)
+    public void ctr_OneNull() {
+        new Complex(BigDecimal.ONE, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void ctr_NullOne() {
+        new Complex(null, BigDecimal.ONE);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void ctr_NullNull() {
+        new Complex(null, null);
+    }
+
+    @Test
+    public void getReal() {
+        Complex target = new Complex(BigDecimal.ONE, BigDecimal.TEN);
+        assertEquals(BigDecimal.ONE, target.getReal());
+    }
+
+    @Test
+    public void getImag() {
+        Complex target = new Complex(BigDecimal.ONE, BigDecimal.TEN);
+        assertEquals(BigDecimal.TEN, target.getImag());
+    }
+}
